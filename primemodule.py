b538eb4138febc23a0825b0ec7b957f85d3aa751
Darren <darrenrohan64@gmail.com>
2024-10-23 16:22:59 -0700

    Finished some, 8.5 is giving me trouble

--- primemodule.py
+++ primemodule.py
@@ -1,24 +1,24 @@
 primecheck = (2, 3, 5, 7, 11)
-def is_prime():
+def is_prime(num):
     i = 0
     while i in range(primecheck):
-        if input % primecheck[i] == 0:
+        if num % primecheck[i] == 0:
             return False
         else:
             i += 1
             if i > 5:
                 return True
-def print_primes():
+def print_primes(num):
     i = 0
     while i in range(primecheck):
-        if input > primecheck[i]:
+        if num > primecheck[i]:
             print(primecheck[i], " ")
         else:
             break
-def get_primes():
+def get_primes(num):
     i = 0
     while i in range(primecheck):
-        if input > primecheck[i]:
+        if num > primecheck[i]:
             return primecheck[i]
         else:
             break
